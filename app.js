3ffda95a575a527227aa7ddfd2ae626f310d71d1
EduardoMoreiraCode <eduu.eduardo09@gmail.com>
2023-08-16 09:58:29 -0300

    feat: add index page styles and darkmode

---
+++ app.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const darkModeStorage = localStorage.getItem('dark-mode');
+    const html = document.querySelector('html');
+    var inputDarkMode = document.getElementById('input-dark-mode');
+    
+    if(darkModeStorage){
+        html.setAttribute("dark", "true");
+        inputDarkMode.checked = true;
+        document.querySelector(".img").src="sun-icon.png";
+    }
+
+    inputDarkMode.addEventListener('change', () => {
+        
+        if(inputDarkMode.checked){
+            html.setAttribute("dark", "true");
+            localStorage.setItem('dark-mode', true);
+            document.querySelector(".img").src="sun-icon.png";
+            
+        }else{
+            html.removeAttribute("dark");
+            localStorage.removeItem('dark-mode');
+            document.querySelector(".img").src="moon-icon.png";
+        }
+    });
+})
